6d44b780f1714eb19c38db8e0dba3a11982bbdab
tonylxc <cooniur@gmail.com>
2013-11-08 19:36:40 -0500

    bug fix

--- dlinkedlist.h
+++ dlinkedlist.h
@@ -38,6 +38,6 @@ int dlinkedlist_remove(dlinkedlist_t *list, dlinkedlist_element_t *e);
 #define dlinkedlist_foreach_element(e, list) \
 	size_t size = dlinkedlist_size(list); \
 	dlinkedlist_element_t *(e) = (list)->first;	\
-	for (int i = 0; i < size; (e) = (e)->next, i++)
+	for (size_t i = 0; i < size; (e) = (e)->next, i++)
 
 #endif
